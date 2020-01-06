e9356a859a509eee1e663722a952624cbe3f2ae2
Rob Ranf <robranf@gmail.com>
2020-01-05 16:39:00 -0800

    adding main content

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      <h1>Emiya Consulting</h1>
+    <div className="bg-primary">
+      <div className="main p-2">
+        <h1 className="main-text">Web and Mobile App Development</h1>
+      </div>
     </div>
   );
 };
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,29 +3,33 @@ import emiyaLogo from "./ec_new_logo_tags_small.svg";
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-primary">
-      <div className="logo-main">
-        <img src={emiyaLogo} alt="" />
-      </div>
-      <p>
-        <a href="/portfolio">Portfolio</a>
-      </p>
-      <p>
-        <a href="/cv">C.V.</a>
-      </p>
-      <p>
-        <a href="/services">Services</a>
-      </p>
-      <p>
-        <a href="/blog">Blog</a>
-      </p>
-      <p>
-        <a href="/contact">Contact</a>
-      </p>
-      <p>
-        <a href="/client">Client Login</a>
-      </p>
-    </nav>
+    <div className="bg-primary">
+      <nav className="navbar p-2">
+        <div className="logo-main">
+          <a href="/">
+            <img src={emiyaLogo} alt="" />
+          </a>
+        </div>
+        <p>
+          <a href="/portfolio">Portfolio</a>
+        </p>
+        <p>
+          <a href="/cv">C.V.</a>
+        </p>
+        <p>
+          <a href="/services">Services</a>
+        </p>
+        <p>
+          <a href="/blog">Blog</a>
+        </p>
+        <p>
+          <a href="/contact">Contact</a>
+        </p>
+        <p>
+          <a href="/client">Client Login</a>
+        </p>
+      </nav>
+    </div>
   );
 };
 
